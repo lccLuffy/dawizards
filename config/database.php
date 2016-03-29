c70e5cde4f02870f6730c4dc79fc341fd37eaeeb
李聪聪 <528360256@qq.com>
2016-03-29 16:51:20 +0800

    init

--- config/database.php
+++ config/database.php
@@ -55,9 +55,9 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => 'dawizards',
-            'username'  => 'root',
-            'password'  => '',
+            'database'  => env('DB_DATABASE',''),
+            'username'  => env('DB_HOST','root'),
+            'password'  => env('DB_PASSWORD',''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
