52a5dd6891afd48eb4682a5bee0b28edf178738a
李聪聪 <528360256@qq.com>
2016-03-30 15:00:09 +0800

    init

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,11 @@
                 @include('partials.errors')
                 @include('partials.success')
                 <div class="panel panel-success">
+                    <div class="panel-heading">
+                        DA wizards
+                    </div>
                     <div class="panel-body">
+
                         <a href="{{ url('impress') }}">
                             <button class="btn btn-info">工作室介绍</button>
                         </a>
@@ -15,27 +19,6 @@
                         </a>
                     </div>
                 </div>
-                {{--@if(Auth::check())
-                    <div class="panel panel-success">
-                        <div class="panel-body">
-                            <a href="{{ url('impress') }}">
-                                <button class="btn btn-info">工作室介绍</button>
-                            </a>
-                            <a href="{{ url('join') }}">
-                                <button class="btn btn-success">加入我们</button>
-                            </a>
-                        </div>
-                    </div>
-
-                @else
-                    <div class="panel panel-default">
-                        <div class="panel-heading">Da Wizards</div>
-                        <div class="panel-body">
-                            <p class="caption">请<a href="{{url('login')}}">登录</a>或者<a href="{{url('register')}}">注册</a>
-                            </p>
-                        </div>
-                    </div>
-                @endif--}}
 
             </div>
         </div>
--- resources/views/join.blade.php
+++ resources/views/join.blade.php
@@ -6,8 +6,16 @@
     <div class="container">
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
-                {{--@include('partials.errors')--}}
                 @include('partials.success')
+                <div class="panel panel-success">
+                    <div class="panel-heading">
+                        注意
+                    </div>
+                    <div class="panel-body">
+                        <h3>4月1日之前填写,5号给出面试名单
+                        </h3>
+                    </div>
+                </div>
                 @include('choose')
             </div>
         </div>
