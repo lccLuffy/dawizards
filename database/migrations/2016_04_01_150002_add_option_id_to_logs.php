bb5a6e5bf6dceeb81b949a31fe1c539344da80b8
李聪聪 <528360256@qq.com>
2016-04-01 15:03:25 +0800

    init

---
+++ database/migrations/2016_04_01_150002_add_option_id_to_logs.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddOptionIdToLogs extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('logs', function (Blueprint $table) {
+            $table->integer('option_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('logs', function (Blueprint $table) {
+            $table->dropColumn('option_id');
+        });
+    }
+}
